b2ded940195013c56bb3c416a969a9f58457fa9f
beeb <703631+beeb@users.noreply.github.com>
2026-02-25 22:30:04 +0200

    chore: add flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      fenix,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlays.default ];
+          };
+          toolchain = fenix.packages.${system}.fromToolchainFile { dir = ./.; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              toolchain
+            ];
+
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          };
+        }
+      );
+    };
+}
